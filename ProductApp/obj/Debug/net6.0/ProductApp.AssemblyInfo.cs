7f8b7a84414db74bbe1a1dec5e808cec737f7ca1
Ioan <ioan.baicoianu@se.com>
2024-09-17 15:13:32 +0200

    added site loc

--- ProductApp/obj/Debug/net6.0/ProductApp.AssemblyInfo.cs
+++ ProductApp/obj/Debug/net6.0/ProductApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fed60e351267d0310e13e8c59a0b1fe486b5e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92645ea6cd900b12b42b66577c9505be9ea55636")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
